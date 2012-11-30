e54e7c96b450b95bfbb2a3ac033349d6f24bd3d9
Biagio Grimaldi <biagio.grimaldi@gmail.com>
2012-11-30 13:21:57 +0100

    tutorial terminated

--- module/Album/test/AlbumTest/Controller/AlbumControllerTest.php
+++ module/Album/test/AlbumTest/Controller/AlbumControllerTest.php
@@ -47,22 +47,23 @@ class AlbumControllerTest extends PHPUnit_Framework_TestCase
 
     public function testDeleteActionCanBeAccessed()
     {
-        $this->routeMatch->setParam('action', 'delete');
-
-        $result   = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
+	    $this->routeMatch->setParam('action', 'delete');
+	    $this->routeMatch->setParam('id', '1');
+	
+	    $result   = $this->controller->dispatch($this->request);
+	    $response = $this->controller->getResponse();
+	
+	    $this->assertEquals(200, $response->getStatusCode());
     }
 
     public function testEditActionCanBeAccessed()
     {
-        $this->routeMatch->setParam('action', 'edit');
-
-        $result   = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-
-        $this->assertEquals(302, $response->getStatusCode());
+	    $this->routeMatch->setParam('action', 'delete');
+	
+	    $result   = $this->controller->dispatch($this->request);
+	    $response = $this->controller->getResponse();
+	
+	    $this->assertEquals(302, $response->getStatusCode());
     }
 
     public function testIndexActionCanBeAccessed()
